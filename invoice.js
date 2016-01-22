85d5bcbc7ab7c7796fd953d18c1edbfc4786c014
adgreen93 <adgreen93@gmail.com>
2016-01-22 15:27:38 -0500

    invoice generator is moving along pretty quick

---
+++ invoice.js
@@ -0,0 +1,26 @@
+// this sets up express, making it easier to start the generator 
+var app = require('express')();
+
+var handle = require('handlebars');
+
+// this enables the server starter to work 
+var http = require('http').Server(app);
+
+var handlebars = require('express-handlebars').create({
+
+		defaultLayout: 'main'
+});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+//this is the link to the main page
+
+app.get('/', function(req, res){
+res.render('home');
+});
+
+//this starts the server 
+http.listen(3000, function(){
+console.log('Invoice Generator is Online!');
+});
